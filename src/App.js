92c87eba026060396443458aea5853c8a893e939
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-23 18:01:05 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
+//Styles
 import './App.css'
 
 //Pages
 import { Home, Rooms, SingleRoom, Error } from './pages'
 
+//Components
+import Navbar from './components/Navbar'
+
 function App() {
-  return <> Hello from app </>
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/" exact component={Rooms} />
+        <Route path="/rooms/:slug" exact component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  )
 }
 
 export default App
